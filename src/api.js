9ae5b13638d85c47777d9f7566e839d820f6548d
Argha Auddy <arghaauddy@Arghas-MacBook-Air.local>
2025-02-26 01:56:44 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "86c275609emsh7f87bd0c77e5338p10c2e6jsn6f2f5ef513c8",  // Use your actual key
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; // Base URL
+
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9af966b877315f7c3fba5bb94eb80862"; // enter your key from openweather API
---
+++ src/components/search/search.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+        geoApiOptions
+      );
+  
+      const data = await response.json();
+  
+      if (!data.data) {
+        throw new Error("No data returned from API");
+      }
+  
+      return {
+        options: data.data.map((city) => ({
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
+        })),
+      };
+    } catch (error) {
+      console.error("Error fetching cities:", error);
+      return { options: [] }; // Return empty options on error
+    }
+  };
+  
+  
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
